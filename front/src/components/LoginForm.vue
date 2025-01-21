2ace3f33b9c2a21ff37d118953dece8d5c34af72
WalidDevIO <walid.elouazizi@etudiant.univ-brest.fr>
2025-01-21 11:39:10 +0100

    Page de co + Thème light

---
+++ front/src/components/LoginForm.vue
@@ -0,0 +1,27 @@
+<template>
+<VContainer class="h-100">
+    <VSheet :elevation="5" class="h-75 pa-5 rounded-lg">
+      <h2 class="mb-2">Se connecter</h2>
+      <VForm>
+        <VTextField v-model="username"/>
+        <VTextField v-model="password" type="password"/>
+        <VBtn @click="login(username, password)">Se connecter</VBtn>
+      </VForm>
+    </VSheet>
+  </VContainer>
+</template>
+
+<script setup>
+import { VTextField } from 'vuetify/components/VTextField';
+import { ref } from 'vue';
+
+const username = ref("")
+const password = ref("")
+
+const props = defineProps({
+    login: {
+        type: Function,
+        required: true
+    }
+})
+</script>
--- front/src/pages/index.vue
+++ front/src/pages/index.vue
@@ -1,7 +1,35 @@
 <template>
-  <HelloWorld />
+  <template v-if="isLogged">
+    <VContainer class="h-100">
+      <VSheet :elevation="5" class="h-75 pa-5 rounded-lg">
+        <h2 class="mb-2">Bienvenue</h2>
+      </VSheet>
+    </VContainer>
+  </template> 
+  <template v-else>
+    <LoginForm :login="login"/>
+  </template>
 </template>
 
 <script setup>
-  //
+import LoginForm from '@/components/LoginForm.vue';
+import { ref } from 'vue';
+
+const isLogged = ref(false)
+
+const login = (username, password) => {
+  console.log(username, password)
+  //Requete API
+  isLogged.value = true
+  fetch("fac/api/coetudiant", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      username: username,
+      password: password
+    })
+  })
+}
 </script>

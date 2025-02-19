7e6eb01dd585bf265701d3b458670a78f083fcef
WalidDevIO <walid.elouazizi@etudiant.univ-brest.fr>
2025-02-20 00:34:05 +0100

    Première implémentation
    - Implémentation endpoint GET /messages/{Numéro/ID etudiant}
    - Ajout fichier web.xml
    - Correction volumes docker compose

--- messagesapi/src/main/java/fr/elite/messagesapi/servlet/MessageServlet.java
+++ messagesapi/src/main/java/fr/elite/messagesapi/servlet/MessageServlet.java
@@ -1,6 +1,11 @@
 package fr.elite.messagesapi.servlet;
 
+import com.mongodb.client.model.Filters;
+
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.bson.types.ObjectId;
 
@@ -8,6 +13,7 @@ import com.mongodb.client.MongoCollection;
 
 import fr.elite.messagesapi.access.MongoAccessSingleton;
 import fr.elite.messagesapi.pojo.Message;
+import fr.elite.messagesapi.utils.MessageJson;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -42,6 +48,9 @@ public class MessageServlet extends HttpServlet {
 
     private void handle(HttpServletRequest request, HttpServletResponse response, String method) {
         String pathInfo = request.getPathInfo();
+        if(pathInfo == null || pathInfo.isEmpty()) {
+            //Handle erreur 400
+        }
         pathInfo = pathInfo.replace("/", "");
 
         if(method == "GET" || method == "POST") {
@@ -57,8 +66,25 @@ public class MessageServlet extends HttpServlet {
 
     }
 
-    private void handleStudentNumberMapping(HttpServletRequest request, HttpServletResponse response, String method, Long studentNumber) {
+    private void handleStudentNumberMapping(HttpServletRequest request, HttpServletResponse response, String method, Long studentNumber) throws IOException {
         // Méthode GET => Renvoyer tableau JSON des messages dont student == studentNumber
+        if(method == "GET") {
+            //Récupérer Message correspondant et les mettre dans une liste
+            List<Message> list = new ArrayList<>();
+            access.find(Filters.eq("student", studentNumber)).forEach(list::add);
+
+            //Préparer les headers de la réponse (CORS et type de contenu)
+            response.addHeader("Access-Control-Allow-Origin", "*");
+            response.addHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, HEAD");
+            response.addHeader("Access-Control-Allow-Headers", "X-PINGOTHER, Origin, X-Requested-With, Content-Type, Accept");
+            response.addHeader("Access-Control-Max-Age", "1728000");
+            response.setContentType("application/json; charset=UTF-8");
+
+            //Ecrire un tableau vide ou contenant les messages sérializés en json séparés par des virgules
+            PrintWriter out = response.getWriter();
+            out.println("[" + list.stream().map(m -> MessageJson.toJson(m)).reduce((acc, c) -> acc + "," + c).orElse("") + "]");
+            out.close();
+        }
         // Méthode POST => Créer un message non lu en fonction du contenu pour l'étudiant
     }
 

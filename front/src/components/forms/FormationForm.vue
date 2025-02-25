fdc9f75bc21d6e401a25cf1cf69d5f83e6889505
WalidDevIO <walid.elouazizi@etudiant.univ-brest.fr>
2025-02-25 22:47:24 +0100

    Formulaire UE et formation
    - Ajout d'un formulaire pour les UEs
    - Ajout d'un formulaire pour les formations
    
    EL OUAZIZI Walid

--- front/src/components/forms/FormationForm.vue
+++ front/src/components/forms/FormationForm.vue
@@ -1,3 +1,30 @@
 <template>
-    
+    <VForm v-model="isFormValid">
+        <VTextField v-model="formation.nom" placeholder="Nom de la formation" :rules="[v => !!v || 'Un nom de formation est requis !']"/>
+
+        <VTextField v-model="formation.responsable" placeholder="Responsable de la formation" :rules="[v => !!v || 'Un reponsable de formation est requis !']"/>
+
+        <VTextField v-model="formation.mailresp" placeholder="Email du responsable de la formation" 
+            :rules="[
+                v => !!v || 'Un mail de responsable de formation est requis !',
+                v => !v || /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'L\'adresse mail du reponsable doit être valide'
+            ]"
+        />
+
+        <VTextField type="number" v-model="formation.nbopt" :rules="[v => Number.isInteger(Number.parseInt(v))  && Number.parseInt(v) >= 0 || 'Ce champs doit être un nombre !']" placeholder="Nombres d'UE optionnelles"/>
+
+        <VTextField type="number" v-model="formation.tdsize" :rules="[v => Number.isInteger(Number.parseInt(v)) && Number.parseInt(v) > 0 || 'Ce champs doit être un nombre !']" placeholder="Taille max des groupes de TD"/>
+
+        <VTextField type="number" v-model="formation.tpsize" :rules="[v => Number.isInteger(Number.parseInt(v)) && Number.parseInt(v) > 0 || 'Ce champs doit être un nombre !']" placeholder="Taille max des groupes de TP"/>
+
+        <VBtn @click.prevent="console.log(JSON.stringify(formation))" class="mt-4" :disabled="!isFormValid">
+            Ajouter la formation
+        </VBtn>
+    </VForm>    
 </template>
+
+<script setup>
+import { ref } from 'vue';
+const isFormValid = ref(false)
+const formation = ref({})
+</script>
--- front/src/components/forms/UeForm.vue
+++ front/src/components/forms/UeForm.vue
@@ -1,3 +1,35 @@
 <template>
-    
+    <VForm v-model="isFormValid">
+        <VTextField v-model="ue.nom" placeholder="Nom de l'UE" :rules="[v => !!v || 'Un nom d\'UE est requis !']"/>
+
+        <VTextField v-model="ue.responsable" placeholder="Responsable de l'UE" :rules="[v => !!v || 'Un reponsable d\'UE est requis !']"/>
+
+        <VRow>
+            <VCheckbox v-model="ue.option" label="Option ?"/>
+        </VRow>
+
+        <template v-if="ue.option">
+            <VLabel for="capacite">
+            Capacité d'accueil de l'UE (-1 pour Infini)
+        </VLabel>
+        <VTextField type="number" id="capacite" v-model="ue.capacite" :rules="[v => Number.isInteger(Number.parseInt(v)) || 'Ce champs doit être un nombre !']"/>
+        </template>
+
+        <VBtn @click.prevent="console.log(JSON.stringify(ue))" class="mt-4" :disabled="!isFormValid">
+            Ajouter l'UE
+        </VBtn>
+    </VForm>    
 </template>
+
+<script setup>
+import { ref, watch } from 'vue';
+const isFormValid = ref(false)
+const ue = ref({})
+
+watch(() => ue.option, (newValue) => {
+    if(!newValue) {
+        delete ue.value.capacite
+        delete ue.value.option
+    }
+})
+</script>

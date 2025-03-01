4f8ec2b11a93a54ff85f65128a3d03dc4301a820
WalidDevIO <walid.elouazizi@etudiant.univ-brest.fr>
2025-03-01 14:25:08 +0100

    Nouvelle logique des DataTable
    EL OUAZIZI Walid

--- front/src/components/formation/DataTableFormation.vue
+++ front/src/components/formation/DataTableFormation.vue
@@ -1,9 +1,17 @@
 <template>
-    <VSheet elevation="5" class="mt-6 h-75 pa-5 rounded-lg w-100">
-        <VContainer class="h-100">
-            <VDataTable class="h-100" :items="formations" :headers="headers">
+    <VSheet elevation="5" class="mt-6 pa-5 rounded-lg w-100">
+        <VContainer>
+            <h2>{{ title }}</h2>
+            <VDataTable :items="formations" :headers="headers">
                 <template v-slot:item.responsable="{item}">
                     <a :href="`mailto:${item.mail}`">{{ item.responsable }}</a>
+                </template>
+                <template v-slot:item.actions="{item}">
+                    <slot name="actions" :item="item">
+                    </slot>
+                </template>
+                <template v-slot:no-data>
+                    Aucune formation disponible
                 </template>
             </VDataTable>
         </VContainer>
@@ -11,37 +19,17 @@
 </template>
 
 <script setup>
-import { computed } from 'vue';
-
-const headers = computed(() => props.detail ? detailHeaders : baseHeaders)
-
-const baseHeaders = [
-    {
-        key: 'nom',
-        title: 'Nom de la formation'
-    },
-    {
-        key: 'responsable',
-        title: 'Responsable de la formation'
-    },
-]
-
-const detailHeaders = [
-    ...baseHeaders,
-    {
-        key: 'nbopt',
-        title: "Nombre d'UE optionnelles à choisir"
-    }
-]
-
 const props = defineProps({
-    detail: {
-        type: Boolean,
-        default: false
-    },
     formations: {
         type: Array,
         default: () => []
-    }
+    },
+    headers: {
+        type: Array,
+    },
+    title: {
+        type: String,
+        required: true
+    },
 })
 </script>
--- front/src/components/formation/DataTableUe.vue
+++ front/src/components/formation/DataTableUe.vue
@@ -1,7 +1,7 @@
 <template>
-    <VSheet elevation="5" class="mt-6 h-75 pa-5 rounded-lg w-100">
+    <VSheet elevation="5" :class="`mt-6 pa-5 rounded-lg w-100 ${props.class}`">
         <VContainer>
-            <h2>Mes enseignements</h2>
+            <h2>{{ title }}</h2>
             <VDataTable :items="ues" :headers="headers">
                 <template v-slot:item.option="{item}">
                     <v-checkbox-btn class="ml-3" v-model="item.option" readonly/>
@@ -10,33 +10,30 @@
                 <template v-slot:item.responsable="{item}">
                     <a :href="`mailto:${item.mail}`">{{ item.responsable }}</a>
                 </template>
+                <template v-slot:no-data>
+                    Aucune UEs disponible
+                </template>
+                <template v-slot:item.actions="{item}">
+                    <slot name="actions" :item="item">
+                    </slot>
+                </template>
             </VDataTable>
         </VContainer>
     </VSheet>
 </template>
 
 <script setup>
-import { ref } from 'vue';
-
 const props = defineProps({
     ues: {
         type: Array,
         default: () => []
+    },
+    headers: {
+        type: Array
+    },
+    title: {
+        type: String,
+        required: true
     }
 })
-
-const headers = ref([
-    {
-        key: 'nom',
-        title: 'Nom de l\' UE'
-    },
-    {
-        key: 'responsable',
-        title: 'Responsable de l\'UE'
-    },
-    {
-        key: 'option',
-        title: 'Optionnelle ?'
-    }
-])
 </script>

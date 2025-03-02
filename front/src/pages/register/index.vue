15c970f00ac0e921b9fc87e6a6caa30afe22f7c8
WalidDevIO <walid.elouazizi@etudiant.univ-brest.fr>
2025-03-02 20:43:06 +0100

    Affichage de la réponse lors de l'inscription et redirection
    EL OUAZIZI Walid

--- front/src/pages/register/index.vue
+++ front/src/pages/register/index.vue
@@ -1,15 +1,49 @@
 <template>
     <AuthForm :auth="register" :registerMode="true"/>
+
+    <VDialog v-model="showDialog">
+        <VCard>
+            <VCardTitle>
+                {{ text !== "Erreur" ? "Inscription réussie" : "Erreur" }}
+            </VCardTitle>
+            <VCardText>
+                {{ text }}
+            </VCardText>
+            <VCardActions class="d-flex justify-end">
+                <VBtn @click="close">Fermer</VBtn>
+            </VCardActions>
+        </VCard>
+    </VDialog>
 </template>
 
 <script setup>
 import { api } from '@/utils/axios';
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
 
-    const register = (data) => {
-        api.post('/students/register', data)
-            .then(r => alert(r.data.username))
-            .catch(err => {
-                alert("Erreur")
-            })
+const showDialog = ref(false)
+const text = ref()
+const router = useRouter()
+
+const register = (data) => {
+    api.post('/students/register', data)
+        .then(r => {
+            showMessage("Votre nom d'utilisateur est : " + r.data.username)
+        })
+        .catch(() => {
+            showMessage("Erreur")
+        })
+}
+
+const close = () => {
+    showDialog.value = false
+    if(text.value !== "Erreur") {
+        router.push('/login')
     }
+}
+
+const showMessage = (message) => {
+    text.value = message
+    showDialog.value = true
+}
 </script>

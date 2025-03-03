f28b1fa98196789415cd86431cc86b32ca9a5685
WalidDevIO <walid.elouazizi@etudiant.univ-brest.fr>
2025-03-03 09:32:51 +0100

    Maj des scripts
    EL OUAZIZI Walid

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+#Script qui build les jar et war de nos 3 APIs
+
 mvn -f messagesapi/pom.xml clean package
 ./authapi/gradlew -p authapi/ clean bootJar
 ./studentapi/gradlew -p studentapi/ clean bootJar
-docker compose up --build

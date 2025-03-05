93235b1a587b71ba7924a090f4eca42bbb554751
WalidDevIO <walid.elouazizi@etudiant.univ-brest.fr>
2025-03-05 15:17:21 +0100

    Changements mineurs
    - Plus besoin de compiler les war et jar
    - Front deprends_on coreapi
    
    EL OUAZIZI Walid

--- build.sh
+++ build.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-#Script qui build les jar et war de nos 3 APIs + Installe les dépendances Node (nécessite pnpm)
-
-mvn -f messagesapi/pom.xml clean package
-./authapi/gradlew -p authapi/ clean bootJar
-./studentapi/gradlew -p studentapi/ clean bootJar
+#Script qui installe les dépendances Node (nécessite pnpm)
 pnpm -C ./expressApi i
 pnpm -C ./front i
